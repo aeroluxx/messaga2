77c74585104f28301eea0767f38232a9d3d85316
aero <aeroluxx@mail.ru>
2019-12-01 23:48:19 +0300

    part1 added

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,33 @@
+// @flow
+// this is an auto generated file. This will be overwritten
+
+export const onCreateJob = `subscription OnCreateJob {
+  onCreateJob {
+    id
+    position
+    rate
+    description
+    owner
+  }
+}
+`;
+export const onUpdateJob = `subscription OnUpdateJob {
+  onUpdateJob {
+    id
+    position
+    rate
+    description
+    owner
+  }
+}
+`;
+export const onDeleteJob = `subscription OnDeleteJob {
+  onDeleteJob {
+    id
+    position
+    rate
+    description
+    owner
+  }
+}
+`;
